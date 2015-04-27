f428b06bf5a468ea2a849d9ad927d5c2949fbb09
antix93 <antix93@gmail.com>
2015-04-27 16:23:51 +0200

    Aggiunta la seconda activity. Sono collegate dal tasto "OK" della schermata LogIn

--- app/src/main/java/com/example/tizi/vvf/MainActivity.java
+++ app/src/main/java/com/example/tizi/vvf/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.tizi.vvf;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
@@ -12,14 +16,20 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        final Button OkButton = (Button) findViewById(R.id.OkBtn);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        OkButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Toast toast = Toast.makeText(getApplicationContext(),"Sei stato autenticato",50);
+                toast.show();
+
+                Intent intent = new Intent(v.getContext(), FireActivity1.class);
+                startActivityForResult(intent, 0);
+            }
+        });
     }
 
 }
