6c70004a45e608f21edbfc9c8bbdc64f742eb852
antix93 <antix93@gmail.com>
2015-05-07 15:39:18 +0200

    Piccoli cambiamenti

---
+++ app/src/main/java/com/example/tizi/vvf/DettUtente.java
@@ -0,0 +1,59 @@
+package com.example.tizi.vvf;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class DettUtente extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dett_utente);
+
+        final Button OkButton = (Button) findViewById(R.id.OkBtn);
+
+        OkButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Intent intent = new Intent(v.getContext(), FireActivity1.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+    public boolean onPrepareOptionsMenu(Menu menu){
+        menu.getItem(0).setEnabled(false);
+        menu.getItem(1).setEnabled(false);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_dett_utente, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        //if (id == R.id.action_settings) {
+        //    return true;
+        //}
+
+        return super.onOptionsItemSelected(item);
+    }
+}
