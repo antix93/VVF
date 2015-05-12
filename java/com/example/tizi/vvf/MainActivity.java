1d5a9225690ed1a0b795d37049563cf4335d0030
imaffo <imaffo93@gmail.com>
2015-05-12 13:45:14 +0200

    sdfghj

---
+++ java/com/example/tizi/vvf/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.tizi.vvf;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button OkButton = (Button) findViewById(R.id.OkBtn);
+
+        OkButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                Toast toast = Toast.makeText(getApplicationContext(),"Sei stato autenticato",Toast.LENGTH_LONG);
+                toast.show();
+
+                Intent intent = new Intent(v.getContext(), FireActivity1.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+    }
+
+}
