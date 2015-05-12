961dc5369d83f39acbbced8e4f088e0737fdecc5
antix93 <antix93@gmail.com>
2015-05-12 14:25:11 +0200

    Piccoli cambiamenti

--- app/src/main/java/com/example/tizi/vvf/Principale1.java
+++ app/src/main/java/com/example/tizi/vvf/Principale1.java
@@ -1,18 +1,128 @@
 package com.example.tizi.vvf;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 
-public class Principale1 extends Activity {
+public class Principale1 extends FragmentActivity {
+
+    CollectionPagerAdapter mCollectionPagerAdapter;
+
+    ViewPager mViewPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principale1);
+
+        // Create an adapter that when requested, will return a fragment
+        // representing an object in
+        // the collection.
+        //
+        // ViewPager and its adapters use support library fragments, so we must
+        // use
+        // getSupportFragmentManager.
+        mCollectionPagerAdapter = new CollectionPagerAdapter(
+                getSupportFragmentManager());
+
+        // Set up action bar.
+        final ActionBar actionBar = getActionBar();
+
+        // Specify that the Home button should show an "Up" caret, indicating
+        // that touching the
+        // button will take the user one step up in the application's hierarchy.
+        //actionBar.setDisplayHomeAsUpEnabled(true);
+
+        // Set up the ViewPager, attaching the adapter.
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mCollectionPagerAdapter);
+    }
+
+    /**
+     * A {@link android.support.v4.app.FragmentStatePagerAdapter} that returns a
+     * fragment representing an object in the collection.
+     */
+    public class CollectionPagerAdapter extends FragmentStatePagerAdapter {
+
+        final int NUM_ITEMS = 2; // number of tabs
+
+        public CollectionPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int i) {
+            Fragment fragment = new TabFragment();
+            Bundle args = new Bundle();
+            args.putInt(TabFragment.ARG_OBJECT, i);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+
+            String tabLabel = null;
+            switch (position) {
+                case 0:
+                    //Titolo delle label nelle tabs
+                    tabLabel = getString(R.string.label1);
+                    break;
+                case 1:
+                    tabLabel = getString(R.string.label2);
+                    break;
+            }
+
+            return tabLabel;
+        }
+    }
+
+    /**
+     * A dummy fragment representing a section of the app, but that simply
+     * displays dummy text.
+     */
+    public static class TabFragment extends Fragment {
+
+        public static final String ARG_OBJECT = "object";
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+            Bundle args = getArguments();
+            int position = args.getInt(ARG_OBJECT);
+
+            int tabLayout = 0;
+            switch (position) {
+                case 0:
+                    tabLayout = R.layout.activity_tab1;
+                    break;
+                case 1:
+                    tabLayout = R.layout.activity_tab2;
+                    break;
+            }
+
+            View rootView = inflater.inflate(tabLayout, container, false);
+
+            return rootView;
+        }
     }
 
 
