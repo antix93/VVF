53c2e4dfb9cb7cfc587b6305f8fee6afbcf8918a
imaffo <imaffo93@gmail.com>
2015-05-13 15:30:46 +0200

    Added Archivio Intervento (stub) Added Keyboard management in CercaIntervento

--- app/src/main/java/com/example/tizi/vvf/MainMenu.java
+++ app/src/main/java/com/example/tizi/vvf/MainMenu.java
@@ -54,7 +54,7 @@ public class MainMenu extends Activity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_fire_activity1, menu);
+        getMenuInflater().inflate(R.menu.menu_main_menu, menu);
         return true;
     }
 
